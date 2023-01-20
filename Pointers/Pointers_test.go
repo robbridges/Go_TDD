e4b04fc0b443331ac2f4295289dd9d07a5fb9bcb
Rob Bridges <Rmb101187@gmail.com>
2023-01-20 13:58:34 -0800

    Minor updates to Pointers test

--- Pointers/Pointers_test.go
+++ Pointers/Pointers_test.go
@@ -9,12 +9,14 @@ func TestWalletDeposit(t *testing.T) {
 			t.Errorf("got %s, but want %s", wallet.balance, btc)
 		}
 	}
+
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
 
 		assertBalance(t, wallet, Bitcoin(10))
 	})
+
 	t.Run("Withdraw no error", func(t *testing.T) {
 		wallet := Wallet{Bitcoin(100)}
 		err := wallet.Withdraw(Bitcoin(20))
@@ -23,6 +25,7 @@ func TestWalletDeposit(t *testing.T) {
 		}
 		assertBalance(t, wallet, Bitcoin(80))
 	})
+
 	t.Run("Withdraw with error", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(5)}
 		err := wallet.Withdraw(Bitcoin(10))
