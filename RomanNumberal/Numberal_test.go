9ffde096d823b3bff700691b716f40929a89cc57
Rob Bridges <Rmb101187@gmail.com>
2023-01-27 07:47:05 -0800

    Roman Numeral completion

--- RomanNumberal/Numberal_test.go
+++ RomanNumberal/Numberal_test.go
@@ -1,40 +1,84 @@
 package RomanNumberal
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+	"testing/quick"
+)
 
-func TestRomanNumbers(t *testing.T) {
-	cases := []struct {
-		description string
-		num         int
-		want        string
+var (
+	cases = []struct {
+		Arabic uint16
+		Roman  string
 	}{
-		{"1 gets converted to I", 1, "I"},
-		{"2 gets converted to II", 2, "II"},
-		{"3 gets convered to III", 3, "III"},
-		{"4 gets converted to IV", 4, "IV"},
-		{"5 gets converted to V", 5, "V"},
-		{"10 gets converted to X", 10, "X"},
-		{"14 gets converted to XIV", 14, "XIV"},
-		{"18 gets converted to XVIII", 18, "XVIII"},
-		{"20 gets converted to XX", 20, "XX"},
-		{"39 gets converted to XXXIX", 39, "XXXIX"},
-		{"40 gets converted to XL", 40, "XL"},
-		{"47 gets converted to XLVII", 47, "XLVII"},
-		{"49 gets converted to XLIX", 49, "XLIX"},
-		{"50 gets converted to L", 50, "L"},
-		{"100 gets converted to C", 100, "C"},
-		{"90 gets converted to XC", 90, "XC"},
+		{Arabic: 1, Roman: "I"},
+		{Arabic: 2, Roman: "II"},
+		{Arabic: 3, Roman: "III"},
+		{Arabic: 4, Roman: "IV"},
+		{Arabic: 5, Roman: "V"},
+		{Arabic: 6, Roman: "VI"},
+		{Arabic: 7, Roman: "VII"},
+		{Arabic: 8, Roman: "VIII"},
+		{Arabic: 9, Roman: "IX"},
+		{Arabic: 10, Roman: "X"},
+		{Arabic: 14, Roman: "XIV"},
+		{Arabic: 18, Roman: "XVIII"},
+		{Arabic: 20, Roman: "XX"},
+		{Arabic: 39, Roman: "XXXIX"},
+		{Arabic: 40, Roman: "XL"},
+		{Arabic: 47, Roman: "XLVII"},
+		{Arabic: 49, Roman: "XLIX"},
+		{Arabic: 50, Roman: "L"},
+		{Arabic: 100, Roman: "C"},
+		{Arabic: 90, Roman: "XC"},
+		{Arabic: 400, Roman: "CD"},
+		{Arabic: 500, Roman: "D"},
+		{Arabic: 900, Roman: "CM"},
+		{Arabic: 1000, Roman: "M"},
+		{Arabic: 1984, Roman: "MCMLXXXIV"},
+		{Arabic: 3999, Roman: "MMMCMXCIX"},
+		{Arabic: 2014, Roman: "MMXIV"},
+		{Arabic: 1006, Roman: "MVI"},
+		{Arabic: 798, Roman: "DCCXCVIII"},
 	}
+)
 
+func TestConvertingToRomanNumerals(t *testing.T) {
 	for _, test := range cases {
-		t.Run(test.description, func(t *testing.T) {
-			got := ConvertToRoman(test.num)
-			want := test.want
-			if got != want {
-				t.Errorf("got %q, want %q", got, want)
+		t.Run(fmt.Sprintf("%d gets converted to '%s", test.Arabic, test.Roman), func(t *testing.T) {
+			got := ConvertToRoman(test.Arabic)
+			if got != test.Roman {
+				t.Errorf("got %q, want %q", got, test.Roman)
 			}
-
 		})
 	}
+}
 
+func TestConvertingToArabic(t *testing.T) {
+	for _, test := range cases {
+		t.Run(fmt.Sprintf("%q gets converted to %d", test.Roman, test.Arabic), func(t *testing.T) {
+			got := ConvertToArabic(test.Roman)
+			if got != test.Arabic {
+				t.Errorf("got %d, want %d", got, test.Arabic)
+			}
+		})
+	}
+}
+
+func TestPropertiesOfConversion(t *testing.T) {
+	assertion := func(arabic uint16) bool {
+		if arabic > 3999 {
+			return true
+		}
+		t.Log("testing", arabic)
+		roman := ConvertToRoman(arabic)
+		fromRoman := ConvertToArabic(roman)
+		return fromRoman == arabic
+	}
+
+	if err := quick.Check(assertion, &quick.Config{
+		MaxCount: 1000,
+	}); err != nil {
+		t.Error("failed checks", err)
+	}
 }
